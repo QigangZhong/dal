b45932c43176e3eef775f3191b7ce400f04cab8b
zhongqigang <zhongqigang@benlai.com>
2018-01-26 19:27:03 +0800

    add test method

--- dal-client/src/test/java/test/com/ctrip/platform/dal/dao/client/dao/DataSourceConfigTest.java
+++ dal-client/src/test/java/test/com/ctrip/platform/dal/dao/client/dao/DataSourceConfigTest.java
@@ -5,12 +5,27 @@ import test.com.ctrip.platform.dal.dao.client.auto.DataSourceConfigDaoAuto;
 import test.com.ctrip.platform.dal.dao.client.entity.DataSourceConfigPojo;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class DataSourceConfigTest {
     @Test
     public void getOne() throws SQLException {
         DataSourceConfigDaoAuto auto=new DataSourceConfigDaoAuto();
         DataSourceConfigPojo result = auto.getOne(41);
+    }
 
+    @Test
+    public void insert() throws SQLException{
+        DataSourceConfigDaoAuto auto=new DataSourceConfigDaoAuto();
+        DataSourceConfigPojo entity=new DataSourceConfigPojo();
+        entity.setStoreSysNo(999);
+        entity.setDataSourceName("testdb");
+        entity.setIdcNetServiceUrl("");
+        entity.setStatus(0);
+        entity.setRemark("testdb remark");
+        Timestamp now=new Timestamp(System.currentTimeMillis());
+        entity.setCreateTime(now);
+        entity.setLastUpdateTime(now);
+        Integer sysNo = auto.insert(entity);
     }
 }
